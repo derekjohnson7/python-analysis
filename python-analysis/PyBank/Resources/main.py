825b64b08d28231794311a758dae5e6a26b0363b
derekjohnson7 <146795367+derekjohnson7@users.noreply.github.com>
2023-12-14 22:21:28 -0600

    Add files via upload

--- python-analysis/PyBank/Resources/main.py
+++ python-analysis/PyBank/Resources/main.py
@@ -3,24 +3,29 @@ import csv
 
 budgetpath = os.path.join("budget_data.csv")
 
+#create empty lists
 months = []
 profits = []
 changes = []
 
 
-
+#open and read the csv file
 with open(budgetpath) as csvfile:
     budgetreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(budgetreader)
+    prev_month = 0
 
-
+#use list comprehension to get our profit and months
     for row in budgetreader:
         months.append(row[0])
         profits.append(int(row[1]))
 
+
+#use list comprehension tp get the change in profits
 for count in range(1, len(profits)):
     changes.append(int(profits[count] - profits[count - 1]))
-    
+
+ #calculate the average change   
 average_change = sum(changes) / len(changes)
 
 print("Financial Analysis")
@@ -31,9 +36,10 @@ print(f'Average Change: ${round(average_change,2)}')
 print(f'Greatest Increase: {months[changes.index(max(changes))+1]} (${max(changes)})')
 print(f'Greatest Decrease: {months[changes.index(min(changes))+1]} (${min(changes)})')
 
-
+#create output path for .txt file
 output_path = os.path.join("analysis","analysis.txt")
 
+#open and write the file
 with open(output_path, 'w') as txtfile:
 
     writer = csv.writer(txtfile)
@@ -43,6 +49,6 @@ with open(output_path, 'w') as txtfile:
     txtfile.write(f'\nTotal Months: {len(months)}')
     txtfile.write(f'\nTotal: $ {sum(profits)}')
     txtfile.write(f'\nAverage Change: {round(average_change,2)}')
-    txtfile.write(f'\nGreatest Increase: {months[changes.index(max(changes))+1]} (${max(changes)})')
-    txtfile.write(f'\nGreatest Decrease: {months[changes.index(min(changes))+1]} (${min(changes)})')
+    txtfile.write(f'\nGreatest Increase: {months[changes.index(max(changes))]} (${max(changes)})')
+    txtfile.write(f'\nGreatest Decrease: {months[changes.index(min(changes))]} (${min(changes)})')
     
--- python-analysis/PyPoll/Resources/main.py
+++ python-analysis/PyPoll/Resources/main.py
@@ -1,24 +1,28 @@
 import os 
 import csv
 
+#create file path
 budgetpath = os.path.join("election_data.csv")
 
-candidate_name={"Charles Casper Stockham", "Diana DeGette", 'Raymon Anthony Doane'}
+#establish variables
+candidate_name={}
 CCS_votes = 0
 DG_votes = 0
 RAD_votes = 0
 total_cast = []
     
 
-
+#open and read the csv file
 with open(budgetpath) as csvfile:
     budgetreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(budgetreader)
 
+    #create a for loop to get the candidates name and add the total votes to the list
     for row in budgetreader:
         candidate_name = row[2]
         total_cast.append(row[0])
 
+        #use if statement to match the number of votes cast to each candidate
         if candidate_name == "Charles Casper Stockham":
             CCS_votes += 1
         elif candidate_name == "Diana DeGette":
@@ -26,13 +30,15 @@ with open(budgetpath) as csvfile:
         elif candidate_name == 'Raymon Anthony Doane':
             RAD_votes +=1
            
-
+        #find the total votes cast
         total_votes = CCS_votes + DG_votes + RAD_votes
 
+        #find the percent of votes cast for each candidate
         CCS_pct = round((CCS_votes/total_votes) * 100,3)
         DG_pct = round((DG_votes/total_votes)* 100, 3)
         RAD_pct = round((RAD_votes/total_votes)*100,3)
 
+    #determine the winner of the popular vote
     if CCS_pct > DG_pct and RAD_pct:
             win = "Charles Casper Stockham"
     elif DG_pct > RAD_pct:
@@ -41,7 +47,7 @@ with open(budgetpath) as csvfile:
             win = "Raymon Anthony Doane"
 
         
-
+#print the results
 print("Election Results")
 print('-------------------------')
 print(f'Total Votes: {len(total_cast)}')   
@@ -52,6 +58,7 @@ print('Raymon Anthony Doane: '+ f'{RAD_pct}% ({RAD_votes})')
 print('-------------------------')
 print(f'Winner: {win}')
 
+#create output path and write into it
 output_path = os.path.join("analysis","analysis.txt")
 
 with open(output_path, 'w') as txtfile:
